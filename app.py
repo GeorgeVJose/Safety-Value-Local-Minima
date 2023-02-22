00747760178ce9474ce53953374722429a89e86b
George V Jose <jorujose@gmail.com>
2023-02-22 16:14:02 +0530

    Updated: Export minima data

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
@@ -10,9 +9,19 @@ def read_data(input_file):
     df = pd.read_csv(input_file)
     return df
 
-st.set_page_config(page_title='Safety Analysis',layout='wide', initial_sidebar_state='auto')
 
-excel_file = st.sidebar.file_uploader('Upload Excel File', type=['csv'])
+@st.cache_data
+def process_data(df, safety_thresh):
+    df = df[df['ACT'] < safety_thresh]
+    peaks, _ = find_peaks(df['ACT']*-1)
+    df_peaks = df.iloc[peaks]
+    return df_peaks
+
+
+st.set_page_config(page_title='Safety Analysis',
+                   layout='wide', initial_sidebar_state='auto')
+
+uploaded_file = st.sidebar.file_uploader('Upload CSV File', type=['csv'])
 st.sidebar.title('Parameters')
 st.sidebar.write('---')
 saftey_thresh = st.sidebar.slider('Safety Value Threshold', 0, 15, 2, 1)
@@ -20,23 +29,28 @@ saftey_thresh = st.sidebar.slider('Safety Value Threshold', 0, 15, 2, 1)
 st.title('Safety Analysis')
 st.write('---')
 
-if excel_file is not None:
-    df = read_data(input_file=excel_file)
+if uploaded_file is not None:
+    df = read_data(uploaded_file)
+    vehicle_id = st.selectbox(
+        "Select vehicle ID", df['VehicleID'].unique())
+    st.header(vehicle_id)
 
-    vehicle_id = st.sidebar.selectbox("Select vehicle ID", df['VehicleID'].unique())
-    df = df[df['VehicleID'] == vehicle_id]
+    df_vehicle = df[df['VehicleID'] == vehicle_id]
+    df_vehicle = df_vehicle[df_vehicle['ACT'] < saftey_thresh]
 
-    df = df[df['ACT'] < saftey_thresh]
+    df_peaks = process_data(df, saftey_thresh)
 
-    # for id_name, df_group in df.groupby('VehicleID'):
-    st.subheader(vehicle_id)
-    peaks, _ = find_peaks(df['ACT']*-1)
+    df_peaks_vehicle = df_peaks[df_peaks['VehicleID'] == vehicle_id]
 
-    peak_x = df['Time'].iloc[peaks]
-    peak_y = df['ACT'].iloc[peaks]
-
-    fig = px.line(df, x='Time', y='ACT', title='Time v/s ACT')
-    fig.add_scatter(x=peak_x, y=peak_y, mode='markers', name='Minima Point', marker_color='red', marker_size=10)
+    fig = px.line(df_vehicle, x='Time', y='ACT', title='Time v/s ACT')
+    fig.add_scatter(
+        x=df_peaks_vehicle['Time'],
+        y=df_peaks_vehicle['ACT'],
+        mode='markers',
+        name='Minima Point',
+        marker_color='red',
+        marker_size=10
+    )
 
     fig.update_xaxes(mirror=True, showline=True, linecolor='black')
     fig.update_yaxes(mirror=True, showline=True, linecolor='black')
@@ -45,27 +59,13 @@ if excel_file is not None:
     with col1:
         st.plotly_chart(fig, use_container_width=True)
     with col2:
-        st.write(df.iloc[peaks])
-        
+        st.write(df_peaks_vehicle)
 
-    # for sheetname, df in data.items():
-    #     # st.write('---')
-    #     st.subheader(sheetname)
-    #     df = df[df['Safety'] < saftey_thresh]
-
-    #     peaks, _ = find_peaks(df['Safety']*-1)
-        
-    #     # Draw plot Time v/s Safety
-    #     peak_x = df['Time'].iloc[peaks]
-    #     peak_y = df['Safety'].iloc[peaks]
-    #     fig = px.line(df, x='Time', y='Safety', title='Time v/s Safety')
-    #     fig.add_scatter(x=peak_x, y=peak_y, mode='markers', name='Minima Point', marker_color='red', marker_size=10)
-    #     # fig.update_layout(showlegend=False)
-    #     fig.update_xaxes(mirror=True, showline=True, linecolor='black')
-    #     fig.update_yaxes(mirror=True, showline=True, linecolor='black')
-
-    #     col1, col2 = st.columns([3, 1])
-    #     with col1:
-    #         st.plotly_chart(fig, use_container_width=True)
-    #     with col2:
-    #             st.table({'Safety': peak_y.values, 'Time': peak_x.values})
+    st.header('Final Minima Data')
+    st.write(df_peaks)
+    st.download_button(
+        'Download Minima Data',
+        df_peaks.to_csv(index=False).encode('utf-8'),
+        file_name='minima_data.csv',
+        mime='text/csv'
+    )
